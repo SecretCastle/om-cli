02f8fcc2564d40553c9afaf19306f2fe0dfabcba
SecretCastle <chenfangzhong@hotmail.com>
2019-06-03 23:24:44 +0800

    upgrade ts file

--- bin/index.js
+++ bin/index.js
@@ -1,158 +1,117 @@
 #!/usr/bin / env node
-
-/**
- * @author SecretCastle
- * @email henrychen9314@gmail.com
- * @create date 2019-06-03 17:29:10
- * @modify date 2019-06-03 17:29:10
- * @desc om-cli 简单的clone github上的脚手架项目
- */
-
-const packageJson = require('../package.json');
-const program = require('commander');
-const colors = require('colors');
-const downloadRep = require('download-git-repo');
-const shell = require('shelljs');
-const ora = require('ora');
-const fs = require('fs')
-
-// 检查node版本，不能低于指定版本
-const version = shell.exec('node --version', { silent: true }).stdout;
+"use strict";
+var packageJson = require('../package.json');
+var program = require('commander');
+var colors = require('colors');
+var downloadRep = require('download-git-repo');
+var shell = require('shelljs');
+var ora = require('ora');
+var fs = require('fs');
+var version = shell.exec('node --version', { silent: true }).stdout;
 if (version <= packageJson.nodeVersion) {
-  console.log(colors.red(`Please Upgrade Your Node Version Above ${packageJson.nodeVersion}`));
-  process.exit(1)
+    console.log(colors.red("Please Upgrade Your Node Version Above " + packageJson.nodeVersion));
+    process.exit(1);
 }
-
-// git url
-const GIT_REMOTE_URL = 'https://github.com:SecretCastle/frontend-webpack-cli'
-// vue
-const VUE_ROOT = '#vue'
-// react
-const REACT_ROOT = '#react'
-// ts
-const TYPESCRIPT_ROOT = '#ts'
-// common
-const COMMON_ROOT = '#common'
-
-let errCount = 0;
-const errTimes = 5;
-
+var GIT_REMOTE_URL = 'https://github.com:SecretCastle/frontend-webpack-cli';
+var VUE_ROOT = '#vue';
+var REACT_ROOT = '#react';
+var TYPESCRIPT_ROOT = '#ts';
+var COMMON_ROOT = '#common';
+var errCount = 0;
+var errTimes = 5;
 program
-  .version(packageJson.version)
-  .usage('om-cli [command] [f]')
-  .option('--vue <f>', 'Create Vue Project')
-  .option('-r, --react <f>', 'Create React Project')
-  .option('-t, --ts <f>', 'Create TypeScript Project')
-  .option('-c, --common <f>', 'Create Common Project')
-
-program.on('--help', () => {
-  console.log(colors.yellow(' \nExample:'))
-  console.log(colors.yellow('   om-cli --ts tsDemo'))
-  console.log(colors.yellow('   om-cli --react reactDemo'))
-  console.log(colors.yellow('   om-cli --vue vueDemo'))
-  console.log(colors.yellow('   om-cli --common Demo \n'))
-})
-
-program.parse(process.argv);
-
-if (process.argv.length === 2) {
-  console.log(colors.green('Please Use "maron-cli --h" to get help'));
-}
-
-/**
- * 初始化根路径
- */
-const argv = require('minimist')(process.argv.slice(2), {
-  'default': {
-    'dir': process.cwd()
-  }
+    .version(packageJson.version)
+    .usage('om-cli [command] [f]')
+    .option('--vue <f>', 'Create Vue Project')
+    .option('-r, --react <f>', 'Create React Project')
+    .option('-t, --ts <f>', 'Create TypeScript Project')
+    .option('-c, --common <f>', 'Create Common Project');
+program.on('--help', function () {
+    console.log(colors.yellow(' \nExample:'));
+    console.log(colors.yellow('   om-cli --ts tsDemo'));
+    console.log(colors.yellow('   om-cli --react reactDemo'));
+    console.log(colors.yellow('   om-cli --vue vueDemo'));
+    console.log(colors.yellow('   om-cli --common Demo \n'));
 });
-
-const root = argv.dir;
-
+program.parse(process.argv);
+if (process.argv.length === 2) {
+    console.log(colors.green('Please Use "maron-cli --h" to get help'));
+}
+var argv = require('minimist')(process.argv.slice(2), {
+    'default': {
+        'dir': process.cwd()
+    }
+});
+var root = argv.dir;
 if (program.vue) {
-  const downloadUrl = GIT_REMOTE_URL + VUE_ROOT
-  initProject(root, downloadUrl, program.vue);
+    var downloadUrl = GIT_REMOTE_URL + VUE_ROOT;
+    initProject({ dir: root, downloadUrl: downloadUrl, filename: program.vue });
 }
 if (program.react) {
-  const downloadUrl = GIT_REMOTE_URL + REACT_ROOT
-  initProject(root, downloadUrl, program.react);
+    var downloadUrl = GIT_REMOTE_URL + REACT_ROOT;
+    initProject({ dir: root, downloadUrl: downloadUrl, filename: program.react });
 }
 if (program.ts) {
-  const downloadUrl = GIT_REMOTE_URL + TYPESCRIPT_ROOT
-  initProject(root, downloadUrl, program.ts);
+    var downloadUrl = GIT_REMOTE_URL + TYPESCRIPT_ROOT;
+    initProject({ dir: root, downloadUrl: downloadUrl, filename: program.ts });
 }
 if (program.common) {
-  const downloadUrl = GIT_REMOTE_URL + COMMON_ROOT
-  initProject(root, downloadUrl, program.common);
+    var downloadUrl = GIT_REMOTE_URL + COMMON_ROOT;
+    initProject({ dir: root, downloadUrl: downloadUrl, filename: program.common });
 }
-
-/**
- * @description 初始化项目
- * @author SecretCastle
- * @date 2019-06-03
- * @param {*} dir 目录路径
- * @param {*} downloadUrl 下载地址
- */
-function initProject(dir, downloadUrl, filename) {
-  const finalPath = dir + '/' + filename
-  // 检查存在指定文件名
-  if (fs.existsSync(finalPath)) {
-    console.log(colors.red('folder already exist'))
-    // 退出
-    process.exit(1)
-  }
-  try {
-    const spinner = ora(colors.blue(`init ${filename} project...`));
-    spinner.start();
-    downloadRep(downloadUrl, finalPath, { clone: true }, err => {
-      spinner.stop();
-      if (err) {
-        console.log(colors.red(err));
-        return;
-      }
-      spinner.succeed(colors.green(`create project ${filename} success`));
-      // 安装依赖
-      installDependencies(filename, finalPath);
-    })
-  } catch (error) {
-    if (error) {
-      errCount += 1;
-      if (errCount < errTimes) {
-        initProject(dir, downloadUrl, filename)
-      } else {
-        console.log(colors.red('Please Try Again later!'))
+function initProject(initParam) {
+    var finalPath = initParam.dir + '/' + initParam.filename;
+    if (fs.existsSync(finalPath)) {
+        console.log(colors.red('folder already exist'));
         process.exit(1);
-      }
     }
-  }
-
+    try {
+        var spinner_1 = ora(colors.blue("init " + initParam.filename + " project..."));
+        spinner_1.start();
+        downloadRep(initParam.downloadUrl, finalPath, { clone: true }, function (err) {
+            spinner_1.stop();
+            if (err) {
+                console.log(colors.red(err));
+                return;
+            }
+            spinner_1.succeed(colors.green("create project " + initParam.filename + " success"));
+            installDependencies({ filename: initParam.filename, finalPath: finalPath });
+        });
+    }
+    catch (error) {
+        if (error) {
+            errCount = 1;
+            if (errCount < errTimes) {
+                initProject(initParam);
+            }
+            else {
+                console.log(colors.red('Please Try Again later!'));
+                process.exit(1);
+            }
+        }
+    }
 }
-
-function installDependencies(filename, finalPath) {
-  if (fs.existsSync(finalPath)) {
-    const spinner = ora(colors.blue('install dependencies...')).start();
-    shell.exec(`cd ${filename} && npm i`, { silent: true }, (err, stdout, stderr) => {
-      if (!err) {
-        spinner.succeed(colors.green('install dependencies succeed'));
-        console.log(
-          colors.rainbow(
-            [
-              `Success! Created ${filename} at ${finalPath}`,
-              'Inside that directory, you can run several commands and more:',
-              '  * npm run dev: Starts you project.',
-              '  * npm test: Run test.',
-              'We suggest that you begin by typing:',
-              `  cd ${filename}`,
-              '  npm start',
-              'Happy hacking!',
-            ].join('\n'),
-          ),
-        );
-      }
-    });
-  } else {
-    console.log(colors.red('System Error, Please Try Again'))
-    process.exit(1);
-  }
+function installDependencies(depParam) {
+    if (fs.existsSync(depParam.finalPath)) {
+        var spinner_2 = ora(colors.blue('install dependencies...')).start();
+        shell.exec("cd " + depParam.filename + " && npm i", { silent: true }, function (err) {
+            if (!err) {
+                spinner_2.succeed(colors.green('install dependencies succeed'));
+                console.log(colors.rainbow([
+                    "Success! Created " + depParam.filename + " at " + depParam.finalPath,
+                    'Inside that directory, you can run several commands and more:',
+                    '  * npm run dev: Starts you project.',
+                    '  * npm test: Run test.',
+                    'We suggest that you begin by typing:',
+                    "  cd " + depParam.filename,
+                    '  npm start',
+                    'Happy hacking!',
+                ].join('\n')));
+            }
+        });
+    }
+    else {
+        console.log(colors.red('System Error, Please Try Again'));
+        process.exit(1);
+    }
 }
---
+++ index.ts
@@ -0,0 +1,177 @@
+#!/usr/bin / env node
+
+/**
+ * @author SecretCastle
+ * @email henrychen9314@gmail.com
+ * @create date 2019-06-03 22:45:33
+ * @modify date 2019-06-03 22:45:33
+ * @desc typescript
+ */
+
+const packageJson = require('../package.json');
+const program = require('commander');
+const colors = require('colors');
+const downloadRep = require('download-git-repo');
+const shell = require('shelljs');
+const ora = require('ora');
+const fs = require('fs')
+
+/**
+ * 定义初始化项目接口
+ */
+interface InitParam {
+  dir: string;
+  downloadUrl: string;
+  filename: string;
+}
+
+/**
+ * 定义初始化依赖接口
+ */
+interface DepParam {
+  filename: string;
+  finalPath: string;
+}
+
+// 检查node版本，不能低于指定版本
+const version = shell.exec('node --version', { silent: true }).stdout;
+if (version <= packageJson.nodeVersion) {
+  console.log(colors.red(`Please Upgrade Your Node Version Above ${packageJson.nodeVersion}`));
+  process.exit(1)
+}
+
+// git url
+const GIT_REMOTE_URL: string = 'https://github.com:SecretCastle/frontend-webpack-cli'
+// vue
+const VUE_ROOT: string = '#vue'
+// react
+const REACT_ROOT: string = '#react'
+// ts
+const TYPESCRIPT_ROOT = '#ts'
+// common
+const COMMON_ROOT: string = '#common'
+
+let errCount: number = 0;
+const errTimes: number = 5;
+
+program
+  .version(packageJson.version)
+  .usage('om-cli [command] [f]')
+  .option('--vue <f>', 'Create Vue Project')
+  .option('-r, --react <f>', 'Create React Project')
+  .option('-t, --ts <f>', 'Create TypeScript Project')
+  .option('-c, --common <f>', 'Create Common Project')
+
+program.on('--help', () => {
+  console.log(colors.yellow(' \nExample:'))
+  console.log(colors.yellow('   om-cli --ts tsDemo'))
+  console.log(colors.yellow('   om-cli --react reactDemo'))
+  console.log(colors.yellow('   om-cli --vue vueDemo'))
+  console.log(colors.yellow('   om-cli --common Demo \n'))
+})
+
+program.parse(process.argv);
+
+if (process.argv.length === 2) {
+  console.log(colors.green('Please Use "maron-cli --h" to get help'));
+}
+
+/**
+ * 初始化根路径
+ */
+const argv = require('minimist')(process.argv.slice(2), {
+  'default': {
+    'dir': process.cwd()
+  }
+});
+
+const root = argv.dir;
+
+if (program.vue) {
+  const downloadUrl: string = GIT_REMOTE_URL + VUE_ROOT
+  initProject({ dir: root, downloadUrl, filename: program.vue });
+}
+if (program.react) {
+  const downloadUrl: string = GIT_REMOTE_URL + REACT_ROOT
+  initProject({ dir: root, downloadUrl, filename: program.react });
+}
+if (program.ts) {
+  const downloadUrl: string = GIT_REMOTE_URL + TYPESCRIPT_ROOT
+  initProject({ dir: root, downloadUrl, filename: program.ts });
+}
+if (program.common) {
+  const downloadUrl: string = GIT_REMOTE_URL + COMMON_ROOT
+  initProject({ dir: root, downloadUrl, filename: program.common });
+}
+
+/**
+ * @description 初始化项目
+ * @author SecretCastle
+ * @date 2019-06-03
+ */
+function initProject(initParam: InitParam) {
+  const finalPath: string = initParam.dir + '/' + initParam.filename
+  // 检查存在指定文件名
+  if (fs.existsSync(finalPath)) {
+    console.log(colors.red('folder already exist'))
+    // 退出
+    process.exit(1)
+  }
+  try {
+    const spinner = ora(colors.blue(`init ${initParam.filename} project...`));
+    spinner.start();
+    downloadRep(initParam.downloadUrl, finalPath, { clone: true }, (err: any) => {
+      spinner.stop();
+      if (err) {
+        console.log(colors.red(err));
+        return;
+      }
+      spinner.succeed(colors.green(`create project ${initParam.filename} success`));
+      // 安装依赖
+      installDependencies({ filename: initParam.filename, finalPath });
+    })
+  } catch (error) {
+    if (error) {
+      errCount = 1;
+      if (errCount < errTimes) {
+        initProject(initParam)
+      } else {
+        console.log(colors.red('Please Try Again later!'))
+        process.exit(1);
+      }
+    }
+  }
+
+}
+
+/**
+ * 
+ * @param depParam 
+ */
+function installDependencies(depParam: DepParam) {
+  if (fs.existsSync(depParam.finalPath)) {
+    const spinner = ora(colors.blue('install dependencies...')).start();
+    shell.exec(`cd ${depParam.filename} && npm i`, { silent: true }, (err: any) => {
+      if (!err) {
+        spinner.succeed(colors.green('install dependencies succeed'));
+        console.log(
+          colors.rainbow(
+            [
+              `Success! Created ${depParam.filename} at ${depParam.finalPath}`,
+              'Inside that directory, you can run several commands and more:',
+              '  * npm run dev: Starts you project.',
+              '  * npm test: Run test.',
+              'We suggest that you begin by typing:',
+              `  cd ${depParam.filename}`,
+              '  npm start',
+              'Happy hacking!',
+            ].join('\n'),
+          ),
+        );
+      }
+    });
+  } else {
+    console.log(colors.red('System Error, Please Try Again'))
+    process.exit(1);
+  }
+}
